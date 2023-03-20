8eff041099fb601c7c424424442697066a590f41
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-20 19:44:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`;
+}
+let saturdayToDo = saturdayFun();
+saturdayFun();
+saturdayFun("bathe my dog");
+
+let mondayWork = function(activity = `go to the office.`) 
+{
+    return `This Monday, I will ${activity}`;
+}
+let mondayToDo = mondayWork();
+mondayWork();
+mondayWork(`work from home.`);
+
+function wrapAdjective(flair = "*") 
+{
+    return function(adj = "special") {
+  
+        return `You are ${flair}${adj}${flair}`
+    }
+    
+}
+let result = wrapAdjective('*');
+let emphatic = result("a hard worker");
+emphatic();
+
+let result2 = wrapAdjective("||");
+let emphatic2 = result("a dedicated programmer")
+emphatic2();
